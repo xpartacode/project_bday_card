d74208b2830f086b98a10be927d645c4ef537b9e
xpartacode <288302482+xpartacode@users.noreply.github.com>
2026-05-27 19:30:03 +0700

    Commit 2

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Birthday card loaded!");
+    // Nanti kita bisa tambahkan logika interaktif di sini, 
+    // seperti tombol untuk memainkan lagu atau animasi confetti.
+});
